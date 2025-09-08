44051c6a2aad5eaf963916af5de00fa0d4b46b91
lakshmip24 <lakshmipradeep2411@gmail.com>
2025-09-08 23:12:38 +0530

    added functionalities

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useRef, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import DsplayHome from "./DsplayHome";
+import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/assets";
 const Display = () => {
+  const displayRef =useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
+
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+    } else {
+      displayRef.current.style.background = "#121212";
+    }
+  });
+
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#181818] text-white overflow-auto lg:w-[75%] lg:ml-0">
+    <div ref={displayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#181818] text-white overflow-auto lg:w-[75%] lg:ml-0">
       <Routes>
         <Route path="/" element={<DsplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
